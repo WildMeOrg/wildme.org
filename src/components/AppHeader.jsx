b496d51299674c32d0bbda8db96aea6dd7678616
Ben Scheiner <brmscheiner@gmail.com>
2020-11-12 23:19:43 -0800

    Add logos, scaffolding for dropdown menus

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import DropDownIcon from '@material-ui/icons/ArrowDropDown';
 import Link from './Link';
+import HeaderMenu from './HeaderMenu';
+import InlineButton from './InlineButton';
+import logo from '../assets/WildMe-Logo-Gradient.png';
+
+const products = ['Catalog', 'Intelligent Agent', 'Wow'];
 
 export default function AppHeader() {
   const theme = useTheme();
+  const [productMenuOpen, setProductMenuOpen] = useState(false);
 
   return (
     <AppBar
       position="fixed"
       style={{
+        background: theme.palette.common.black,
         zIndex: theme.zIndex.drawer + 1,
         transition: theme.transitions.create(['width', 'margin'], {
           easing: theme.transitions.easing.sharp,
@@ -27,17 +37,56 @@ export default function AppHeader() {
           margin: '0 auto',
         }}
       >
-        <Link noUnderline href="/">
+        <Link style={{ display: 'flex', alignItems: 'center' }} noUnderline href="/">
+          <img style={{ height: 40 }} src={logo} alt="Wild Me gradient logo" />
           <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
+            component="h5"
+            variant="h5"
             noWrap
-            style={{ fontSize: 20, flexGrow: 1 }}
+            style={{ marginLeft: 12, fontSize: 20, flexGrow: 1, color: theme.palette.common.white }}
           >
             Wild Me
           </Typography>
         </Link>
+        <div
+          style={{ color: theme.palette.common.white, flexShrink: 0 }}
+        >
+          <InlineButton
+            noUnderline
+            style={{
+              textTransform: 'unset',
+              fontSize: 16,
+              letterSpacing: '0.04em',
+              display: 'flex',
+              alignItems: 'center',
+              color: theme.palette.common.white,
+            }}
+            onClick={() => setProductMenuOpen(!productMenuOpen)}
+          >
+            Products
+            <DropDownIcon />
+          </InlineButton>
+          <HeaderMenu
+            open={productMenuOpen}
+            itemCount={3}
+            style={{ right: 12 }}
+          >
+            <MenuList onClick={() => setProductMenuOpen(false)}>
+              {products.map(product => (
+                <Link
+                  key={product}
+                  noUnderline
+                  href="/"
+                  style={{ width: '100%' }}
+                >
+                  <MenuItem className="dark-menu-item">
+                    <Typography>{product}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </MenuList>
+          </HeaderMenu>
+        </div>
       </Toolbar>
     </AppBar>
   );
--- src/pages/splash/Splash.jsx
+++ src/pages/splash/Splash.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import logo from '../../assets/WildMe-Logo-white.png';
 
 export default function Splash() {
   const theme = useTheme();
-
-  console.log(theme);
 
   return (
     <div style={{ textAlign: 'center', marginTop: 64 }}>
@@ -17,14 +16,15 @@ export default function Splash() {
           width: '100%',
           height: '90vh',
           justifyContent: 'center',
+          alignItems: 'center',
           color: theme.palette.common.white,
           background: 'linear-gradient(90deg, #21BDC1, #41D06A)',
           backgroundSize: '400% 400%',
           animation: 'gradient 10s ease infinite',
         }}
       >
-        <Typography variant="h1">Wild Me</Typography>
-        <Typography variant="h4" style={{ padding: '0 8px' }}>
+        <img style={{ width: '40%' }} src={logo} alt="Wild Me logo" />
+        <Typography variant="h6" style={{ fontSize: 16 }}>
           <FormattedMessage id="SPLASH_TAGLINE" />
         </Typography>
       </div>
