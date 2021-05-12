6d326436b1b181d98aae38278d5f03d54f834849
Ben Scheiner <brmscheiner@gmail.com>
2021-05-12 15:33:03 -0700

    Upgrade turtle photos

--- src/pages/platforms/PlatformPage.jsx
+++ src/pages/platforms/PlatformPage.jsx
@@ -16,6 +16,7 @@ export default function PlatformPage({ data }) {
     href,
     descriptionId,
     bannerLarge,
+    bannerSmall,
     sightings,
     partners,
     algorithms,
@@ -89,7 +90,7 @@ export default function PlatformPage({ data }) {
               width: '85%',
               position: 'relative',
             }}
-            src={bannerLarge}
+            src={bannerSmall}
             width="100%"
             alt={`Banner for ${name}`}
           />
