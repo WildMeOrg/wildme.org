516084d1beef0badadb192bfe818d2b06f26a6bf
Ben Scheiner <brmscheiner@gmail.com>
2021-08-17 15:39:11 -0700

    Add chuck

--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -3,6 +3,7 @@ import { Page } from '../../components/Containers';
 import PeopleGallery from './PeopleGallery';
 import board from './board';
 import staff from './staff';
+import advisors from './advisors';
 
 export default function People() {
   return (
@@ -12,6 +13,10 @@ export default function People() {
       subtitle="Our team works at the intersection of conservation, AI, engineering, and academia. We take pride in what we do, and even more pride in how we treat each other."
     >
       <PeopleGallery people={board} titleId="PAGE_TITLE_BOARD" />
+      <PeopleGallery
+        people={advisors}
+        titleId="PAGE_TITLE_ADVISORS"
+      />
       <PeopleGallery people={staff} titleId="PAGE_TITLE_STAFF" />
     </Page>
   );
---
+++ src/pages/team/advisors.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import chuck from '../../assets/staff/chuck.jpg';
+
+export default [
+  {
+    img: chuck,
+    name: 'Dr. Chuck Stewart',
+    role: 'Advisor, Co-founder',
+    renderDescription: () => (
+      <div>
+        <Typography>
+          Dr. Chuck Stewart is Professor and Head of the Department of
+          Computer Science at Rensselaer Polytechnic Institute in
+          Troy, NY. He is also Founder and Chief Scientist at
+          DualAlign LLC. An expert on computer vision and machine
+          learning, he oversaw the development of the IBEIS system, a
+          precursor to the current Wildbook, by his graduate students
+          Jon Crall, Jason Parham and Hendrik Weideman. He is
+          currently involved in Wildbook as an advisor and consultant,
+          helping the team design, build and deploy advanced
+          algorithms for individual animal identification. He is
+          passionate about the development of cutting-edge computer
+          vision algorithms for important applications in biology,
+          ecology, and conservation. Stewart holds a BS in
+          Mathematical Sciences from Williams College and MS and Ph.D.
+          degrees from the University of Wisconsin, Madison.
+        </Typography>
+      </div>
+    ),
+  },
+];
